95c04b1e1dd8e9328f038cc3c3aa36b4de63332d
Shivam Sood <shivamsood16@gmail.com>
2022-06-03 14:07:46 +0530

    creates CourseApiApp class and configured Pom.xml to start springboot app

---
+++ src/main/java/com/Shivam/CourseApiApp.java
@@ -0,0 +1,11 @@
+package com.Shivam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+    public static void Main(String[] args){
+        SpringApplication.run(CourseApiApp.class, args);
+    }
+}
